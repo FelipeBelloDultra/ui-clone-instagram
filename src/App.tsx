9a185043e1757c3f1d5e98de216ff90cef7ea855
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-14 17:57:42 -0300

    feat: create global styles with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles';
+
 const App: React.FC = () => {
-  return <div />;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </>
+  );
 };
 
 export default App;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  *, *:after, *:before {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #FAFAFA;
+    color: #262626;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: sans-serif;
+  }
+
+  button: {
+    cursor: pointer;
+  }
+
+  #root {
+    height: 100vh;
+  }
+`;
+
+export default GlobalStyles;
