ce1c3632999b543f37be4dc30f3475076452ca28
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-15 18:31:40 -0300

    feat: create user info component

---
+++ src/components/UserInfo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const UserInfo: React.FC = () => {
+  return (
+    <Container>
+      <h1>UserInfo</h1>
+    </Container>
+  );
+};
+
+export default UserInfo;
---
+++ src/components/UserInfo/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: fixed;
+  width: 324px;
+  height: 492px;
+  margin-top: 30px;
+  padding: 20px 0 0 30px;
+`;
